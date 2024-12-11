e3ab8151b3ac15766d3f1417d652c8496aa9a609
Jian Yin <pupillight@gmail.com>
2024-12-10 19:48:11 -0500

    Revert "2" from 3
    This reverts commit f1908087d36ed63e95dc5abc588d19354c51d801.

--- src/org/yj/leetcode/LeetCode1482.java
+++ src/org/yj/leetcode/LeetCode1482.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class LeetCode1482 {
 
+
     public static void main(String[] args) {
+        LeetCode1482 instance = new LeetCode1482();
+        int[][] relation = {{0, 2}, {2, 1}, {3, 4}, {2, 3}, {1, 4}, {2, 0}, {0, 4}};
+
+        System.out.println(instance.numWays(5, relation, 3));
 
         System.out.println("--------");
     }
