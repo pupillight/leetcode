ecbe2bc3b90f60912f054c3b570a289678f341af
Jian Yin <pupillight@gmail.com>
2024-12-11 01:51:22 -0500

    99

--- src/org/yj/leetcode/LeetCode1482.java
+++ src/org/yj/leetcode/LeetCode1482.java
@@ -8,6 +8,6 @@ public class LeetCode1482 {
     public static void main(String[] args) {
         System.out.println("11111111");
         System.out.println("22222222");
-        System.out.println("88888");
+        System.out.println("99999999");
     }
 }
